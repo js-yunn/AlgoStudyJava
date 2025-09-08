2f2fc7cce3a6282044cf7ed143ee8af2475ff449
name <name@email.com>
2025-09-08 11:22:16 +0900

    0904 부등호

---
+++ baekjoon/bj_2529_부등호/baekjoon/Main_0904.java
@@ -0,0 +1,54 @@
+package baekjoon;
+import java.io.*;
+import java.util.*;
+
+public class Main_0904 {
+	static int K;
+	static char arr[];
+	static boolean v[] = new boolean[10];
+	static ArrayList<String> answer = new ArrayList<>();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		K = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 부등호 배열 입력받기
+		arr = new char[K];
+		for (int i=0; i<K; i++) {
+			arr[i]=st.nextToken().charAt(0);
+		}
+		
+		// 숫자 10개에 대해 모두 dfs 진행, v배열 업데이트
+		for (int i=0; i<10; i++) {
+			v[i]= true;
+			dfs(i, 0, i+"");
+			v[i]=false;
+		}
+		
+		// 출력
+		System.out.println(answer.get(answer.size()-1));
+		System.out.println(answer.get(0));
+	}
+	
+	static void dfs(int now, int cnt, String num) {
+		if (cnt==K) {
+			answer.add(num);
+			return;
+		}
+		for (int next=0; next<10; next++) {
+			if (!v[next]) {
+				if ((arr[cnt]=='<' && now<next) || (arr[cnt]=='>' && now>next)) {
+					v[next]=true;
+					dfs(next, cnt+1, num+next);
+					v[next]=false;
+				}
+			}
+		}
+		
+	}
+}
+
+/*
+완전탐색이면 어차피 10! 이 최대이므로 가능함.
+백트래킹 조건 cnt == K
+
+*/
